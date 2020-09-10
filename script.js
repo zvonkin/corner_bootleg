be0c605297be2574014ed740fae1ad602c4d3722
zvonkin <mzvx8@mst.edu>
2020-09-09 22:34:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ window.onload = () => {
 function staticLoadPlaces() {
     return [
         {
-            name: 'Pokèmon',
+            name: 'Rick Roll',
             location: {
-                // lat: <your-latitude>,
-                // lng: <your-longitude>,
+                lat: 37.377264,
+                lng: -122.033087
             },
         },
     ];
@@ -20,47 +20,24 @@ function staticLoadPlaces() {
 
 var models = [
     {
-        url: './assets/magnemite/scene.gltf',
-        scale: '0.5 0.5 0.5',
         info: 'Magnemite, Lv. 5, HP 10/10',
-        rotation: '0 180 0',
     },
     {
-        url: './assets/articuno/scene.gltf',
-        scale: '0.2 0.2 0.2',
-        rotation: '0 180 0',
         info: 'Articuno, Lv. 80, HP 100/100',
     },
     {
-        url: './assets/dragonite/scene.gltf',
-        scale: '0.08 0.08 0.08',
-        rotation: '0 180 0',
         info: 'Dragonite, Lv. 99, HP 150/150',
     },
 ];
 
 var modelIndex = 0;
-var setModel = function (model, entity) {
-    if (model.scale) {
-        entity.setAttribute('scale', model.scale);
-    }
-
-    if (model.rotation) {
-        entity.setAttribute('rotation', model.rotation);
-    }
-
-    if (model.position) {
-        entity.setAttribute('position', model.position);
-    }
-
-    entity.setAttribute('gltf-model', model.url);
-
+var setModel = function (model) {
     const div = document.querySelector('.instructions');
     div.innerText = model.info;
 };
 
 function renderPlaces(places) {
-    let scene = document.querySelector('a-scene');
+    let link = document.querySelector('a-link');
 
     places.forEach((place) => {
         let latitude = place.location.lat;
