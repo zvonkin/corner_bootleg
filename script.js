be666117eb942a957ea07275add3a45ce124cd38
zvonkin <mzvx8@mst.edu>
2020-09-09 23:20:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     const button = document.querySelector('button[data-action="change"]');
-    button.innerText = '﹖';
+    button.innerText = 'Select The Link';
 
     let places = staticLoadPlaces();
     renderPlaces(places);
