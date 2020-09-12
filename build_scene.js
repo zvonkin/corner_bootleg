b06af5799cc81f91e3ed02623a8d9934f3258a18
zvonkin <mzvx8@mst.edu>
2020-09-11 21:44:58 -0700

    Update build_scene.js

--- build_scene.js
+++ build_scene.js
@@ -15,25 +15,25 @@ AFRAME.registerComponent("clickable", {
 var tokens = [
   {
     name: 'Paden',
-    imageTop: './assets/paden.jpg'
-    imageBottom: './assets/paden-down.jpg'
-    imageSide: './assets/gold.jpeg'
+    imageTop: './assets/paden.jpg',
+    imageBottom: './assets/paden-down.jpg',
+    imageSide: './assets/gold.jpeg',
     location: {
       lat: 37.377053, ,
       lng: -122.032437,
     },
-    link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+    link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
   },
   {
     name: 'Misha',
-    imageTop: './assets/misha.jpg'
-    imageBottom: './assets/misha-down.jpg'
-    imageSide: './assets/green.jpg'
+    imageTop: './assets/misha.jpg',
+    imageBottom: './assets/misha-down.jpg',
+    imageSide: './assets/green.jpg',
     location: {
       lat: 37.376723,
       lng: -122.033664,
     },
-    link: 'https://www.youtube.com/watch?v=2-8gsWZqDBM'
+    link: 'https://www.youtube.com/watch?v=2-8gsWZqDBM',
   }, 
 ];
 
