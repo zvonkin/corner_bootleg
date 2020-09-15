e042a24300ad6ba655cd566a5af6103d64697fd0
zvonkin <mzvx8@mst.edu>
2020-09-14 19:56:46 -0700

    Update build_scene.js

--- build_scene.js
+++ build_scene.js
@@ -1,33 +1,9 @@
-import * as componets from './components.js';
+import * as componets from "./components.js";
+import {tokens} from ".tokens.js";
 
 window.onload = () => {
     buildScene(tokens);
 };
-
-var tokens = [
-  {
-    name: "Paden",
-    imageTop: "./assets/paden.jpg",
-    imageBottom: "./assets/paden-down.jpg",
-    imageSide: "./assets/gold.jpeg",
-    location: {
-      lat: 37.377053,
-      lng: -122.032437,
-    },
-    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-  },
-  {
-    name: "Misha",
-    imageTop: "./assets/misha.jpg",
-    imageBottom: "./assets/misha-down.jpg",
-    imageSide: "./assets/green.jpg",
-    location: {
-      lat: 37.376723,
-      lng: -122.033664,
-    },
-    link: "https://www.youtube.com/watch?v=2-8gsWZqDBM",
-  }, 
-];
 
 var getAssetId = function (src) {
   return src.split("/")[2].split(".")[0];
