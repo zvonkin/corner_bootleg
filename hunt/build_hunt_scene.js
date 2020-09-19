6363f1b4a965c1f91f3826ab2d892b63e5a7dc27
zvonkin <mzvx8@mst.edu>
2020-09-18 19:37:55 -0700

    Update build_hunt_scene.js

--- hunt/build_hunt_scene.js
+++ hunt/build_hunt_scene.js
@@ -1,14 +1,4 @@
-
-AFRAME.registerComponent("check-in", {
-  schema: {
-    link: {type: "string", default: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
-  },
-  init: function() {
-    this.el.addEventListener("click", () => {
-      window.open(this.data.link);
-    });
-}});
-
+// Data
 var checkpoints = [
   {
     clue: "Visit Rachael & Vanessa.",
@@ -62,33 +52,93 @@ var checkpoints = [
 
 ];
 
-window.onload = () => {
-    buildScene(tokens);
-};
+var mixins = [
+  // Geometries
+  {
+    id: "inner-token", 
+    kv: [
+      {key: "material", value: "color: #6d24b5;"},
+      {key: "geometry", value: "primitive: cylinder; radius: 0.45; height: 0.02;"},
+      {key: "rotation", value: "0 0 90"},
+   ],
+  },
+  {
+    id: "outter-token", 
+    kv: [
+      {key: "material", value: "color: #e3ca3d"},
+      {key: "geometry", value: "primitive: torus; radius: 0.5; radius-tubular: 0.01;"},
+   ],
+  },
+  // Animation
+  {
+    id: "spin", 
+    kv: [
+      {key: "animation", value: "property: rotation; from: 0 0 90; to: 0 360 90; dur: 3000; loop: true; easing:linear;"},
+   ],
+  },  
+];
 
-var buildCheckpoint = function (checkpoint, assets) {
-  let entity = document.createElement("a-cylinder");
+var createMixin = function (mixin) {
+  let a_mixin = document.createElement("a-mixin");
+  a_mixin.setAttribute("id", mixin.id);
+  mixin.kv.ForEach((kv) => {
+    a_mixin.setAttribute(kv.key, kv.value);
+  });
+  return a_mixin;  
+}
+
+var loadMixins = function (mixins) {
+  let assets = document.querySelector("a-assets");
+  mixins.forEach((mixin) => {
+      let asset = createMixin(mixin);
+      assests.appendChild(asset);
+  });  
+}
+
+// Components
+AFRAME.registerComponent("check-in", {
+  schema: {
+    link: {type: "string", default: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
+  },
+  init: function() {
+    this.el.addEventListener("click", () => {
+      window.open(this.data.link);
+    });
+}});
+
+// Functions
+var buildToken = function (checkpoint) {
+  let entity = document.createElement("a-entity");
   
-  let latitude = token.location.lat;
-  let longitude = token.location.lng;
+  let latitude = checkpoint.location.lat;
+  let longitude = checkpoint.location.lng;
   entity.setAttribute("gps-entity-place", `latitude: ${latitude}; longitude: ${longitude};`);  
-  entity.setAttribute("radius", "4");
-  entity.setAttribute("height", "0.5");
-  entity.setAttribute("rotation", "0 0 90");
-  entity.setAttribute("shadow", "");
-  entity.setAttribute("animation", "property: rotation; dur: 3000; from: 0 0 90; to: 0 360 90; loop: true; easing: linear;");
+  
+  let inner = document.createElement("a-entity");
+  inner.SetAttribute("mixin", "inner-token");
+
+  let outer = document.createElement("a-entity");
+  inner.SetAttribute("mixin", "outer-token");
+  
+  entity.appendChild(inner);
+  entity.appendChild(outer);
+
+  entity.setAttribute("mixin", "spin");
   entity.setAttribute("check-in", "");
+  
   return entity
 };
 
-function buildScene(tokens) {
-    let scene = document.querySelector("a-scene");
-    
-    let assets = document.querySelector("a-assets");
-    tokens.forEach((token) => {
-        let entity = buildToken(token, assets);
-        scene.appendChild(entity);
-    });
-
-    scene.appendChild(assets);
+function start(tokens) {
+  let scene = document.querySelector("a-scene");
+  checkpoints.forEach((checkpoint) => {
+      let entity = buildToken(checkpoint);
+      scene.appendChild(entity);
+  });
 }
+
+// Run
+window.onload = () => {
+    loadMixins();
+    buildScene(tokens);
+};
